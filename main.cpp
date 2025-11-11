c11ee96a6eead6c09f92f2f19bf4e540f2a7c4de
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-11-11 10:12:51 -0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define FMT_HEADER_ONLY
+#include <fmt/core.h>
+
+int main() {
+
+    std::cout << "hello\n";
+    fmt::println("world!");
+    return 0;
+}
---
+++ update_libs.ps1
@@ -0,0 +1,2 @@
+$env:VCPKG_ROOT = "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\vcpkg"
+$env:PATH = "$env:VCPKG_ROOT;$env:PATH"
